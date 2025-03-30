8180a5893276e2258a0e1553db38505130d5c8ae
unknown <romulo.gamer.br@gmail.com>
2025-03-30 19:54:59 -0300

    Adição de botões pra filtrar a categoria

--- src/components/Vending_Machine.tsx
+++ src/components/Vending_Machine.tsx
@@ -10,11 +10,17 @@ import Pepsi from './Pepsi.png'
 import hersheys from './hersheys.png'
 import garoto from './garoto.png'
 import '../App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Vending_Machine: React.FC = () => {
-  const handleButtonClick = (imageName: string) => {
-    alert(`${imageName} clicada!`);
+  const [selectedCategory, setSelectedCategory] = useState<string>(''); // Estado para a categoria selecionada
+
+  const handleCategoryClick = (category: string) => {
+    setSelectedCategory(category); // Define a categoria selecionada
+  };
+
+  const handleButtonClick = (productName: string) => {
+    alert(`${productName} clicada!`);
   };
 
   return (
@@ -29,133 +35,328 @@ const Vending_Machine: React.FC = () => {
         }} 
       />
       
-      <ImageButton 
-        src={Doritos} 
-        alt="Imagem do Doritos"
-        style={{
-          position: 'absolute',
-          width: '5%',
-          left: '54%',
-          top: '6%', 
-          transform: 'translateX(-50%)',
-          outline: 'none',
-        }}
-        className="image-button"
-        onClick={() => handleButtonClick('Doritos')}
-      />
-      <ImageButton 
-        src={fandangos} 
-        alt="Imagem do fandangos"
-        style={{
-          position: 'absolute',
-          width: '5%',
-          left: '49%',
-          top: '6%', 
-          transform: 'translateX(-50%)',
-        }}
-        className="image-button"
-        onClick={() => handleButtonClick('fandangos')} 
-      />
-      <ImageButton 
-        src={Cheetos} 
-        alt="Imagem do Cheetos"
-        style={{
-          position: 'absolute',
-          width: '5%',
-          left: '44%',
-          top: '6%', 
-          transform: 'translateX(-50%)',
-        }}
-        className="image-button"
-        onClick={() => handleButtonClick('Cheetos')} 
-      />
+{/* Botões para selecionar a categoria */}
+    <div 
+      style={{ marginBottom: '20px',
+        textAlign: 'center' }}>
+      <button onClick={() => handleCategoryClick('Salgadinhos')} 
+      style={{
+        width: '25px', // Define uma largura específica
+        height: '25px', // Define uma altura igual à largura para um círculo perfeito
+        padding: '0', // Remove o padding extra
+        margin: '0 10px',
+        left:'57.8%',
+        position:'absolute',
+        backgroundColor: '#FF0000',
+        top: '44%',
+        color: 'white',
+        border: 'none',
+        borderRadius: '50%', // Faz o botão redondo
+        cursor: 'pointer'
+    }}
+  >A
+      </button>
+      <button onClick={() => handleCategoryClick('Bebidas')} 
+      style={{
+      width: '25px', // Define uma largura específica
+      height: '25px', // Define uma altura igual à largura para um círculo perfeito
+      padding: '0', // Remove o padding extra
+      margin: '0 10px',
+      left:'59.8%',
+      position:'absolute',
+      backgroundColor: '#FFEB3B',
+      top: '44%',
+      color: 'white',
+      border: 'none',
+      borderRadius: '50%', // Faz o botão redondo
+      cursor: 'pointer'
+  }}
+>B
+</button>
 
-      <ImageButton 
-        src={Monster} 
-        alt="Imagem do Monster"
-        style={{
-          position: 'absolute',
-          width: '5%',
-          left: '54%',
-          top: '18%',
-          transform: 'translateX(-50%)',
-        }}
-        className="image-button"
-        onClick={() => handleButtonClick('Monster')} 
-      />
+    <button onClick={() => handleCategoryClick('Chocolates')} 
+      style={{
+        width: '25px', // Define uma largura específica
+        height: '25px', // Define uma altura igual à largura para um círculo perfeito
+        padding: '0', // Remove o padding extra
+        margin: '0 10px',
+        left:'61.7%',
+        position:'absolute',
+        backgroundColor: '#008000',
+        top: '44%',
+        color: 'white',
+        border: 'none',
+        borderRadius: '50%', // Faz o botão redondo
+        cursor: 'pointer'
+    }}
+  >
+    C
+    </button>
+</div>
 
-<ImageButton 
-        src={Coca} 
-        alt="Imagem do Pepsi"
-        style={{
-          position: 'absolute',
-          width: '6%',
-          left: '49%',
-          top: '18%',
-          transform: 'translateX(-50%)',
-        }}
-        className="image-button"
-        onClick={() => handleButtonClick('Pepsi')} 
-      />
-      
-<ImageButton 
-        src={Pepsi} 
-        alt="Imagem do Pepsi"
-        style={{
-          position: 'absolute',
-          width: '6%',
-          left: '44%',
-          top: '20%',
-          transform: 'translateX(-50%)',
-        }}
-        className="image-button"
-        onClick={() => handleButtonClick('Pepsi')} 
-      />
+      {/* Exibindo TODOS os produtos */}
+      {selectedCategory === '' && (
+        <>
+          {/* Produtos de Salgadinhos */}
+          <ImageButton 
+            src={Doritos} 
+            alt="Imagem do Doritos"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '54%',
+              top: '6%', 
+              transform: 'translateX(-50%)',
+              outline: 'none',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Doritos')}
+          />
+          <ImageButton 
+            src={fandangos} 
+            alt="Imagem do Fandangos"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '49%',
+              top: '6%', 
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Fandangos')} 
+          />
+          <ImageButton 
+            src={Cheetos} 
+            alt="Imagem do Cheetos"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '44%',
+              top: '6%', 
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Cheetos')} 
+          />
 
-      <ImageButton 
-        src={Lacta} 
-        alt="Imagem do Lacta"
-        style={{
-          position: 'absolute',
-          width: '5%',
-          left: '54%',
-          top: '32%',
-          transform: 'translateX(-50%)',
-        }}
-        className="image-button"
-        onClick={() => handleButtonClick('Lacta')} 
-      />
-      <ImageButton 
-        src={hersheys} 
-        alt="Imagem do hersheys"
-        style={{
-          position: 'absolute',
-          width: '5%',
-          left: '49%',
-          top: '32%',
-          transform: 'translateX(-50%)',
-        }}
-        className="image-button"
-        onClick={() => handleButtonClick('hersheys')} 
-      />
-    <ImageButton 
-        src={garoto} 
-        alt="Imagem do garoto"
-        style={{
-          position: 'absolute',
-          width: '5%',
-          left: '44%',
-          top: '32%',
-          transform: 'translateX(-50%)',
-        }}
-        className="image-button"
-        onClick={() => handleButtonClick('garoto')} 
-      />
+          {/* Produtos de Bebidas */}
+          <ImageButton 
+            src={Monster} 
+            alt="Imagem do Monster"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '54%',
+              top: '18%',
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Monster')} 
+          />
+          <ImageButton 
+            src={Coca} 
+            alt="Imagem do Coca"
+            style={{
+              position: 'absolute',
+              width: '6%',
+              left: '49%',
+              top: '18%',
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Coca')} 
+          />
+          <ImageButton 
+            src={Pepsi} 
+            alt="Imagem do Pepsi"
+            style={{
+              position: 'absolute',
+              width: '6%',
+              left: '44%',
+              top: '20%',
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Pepsi')} 
+          />
 
+          {/* Produtos de Chocolates */}
+          <ImageButton 
+            src={Lacta} 
+            alt="Imagem do Lacta"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '54%',
+              top: '32%',
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Lacta')} 
+          />
+          <ImageButton 
+            src={hersheys} 
+            alt="Imagem do Hersheys"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '49%',
+              top: '32%',
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Hersheys')} 
+          />
+          <ImageButton 
+            src={garoto} 
+            alt="Imagem do Garoto"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '44%',
+              top: '32%',
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Garoto')} 
+          />
+        </>
+      )}
 
+      {/* Exibindo os produtos conforme a categoria selecionada */}
+      {selectedCategory === 'Salgadinhos' && (
+        <>
+          <ImageButton 
+            src={Doritos} 
+            alt="Imagem do Doritos"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '54%',
+              top: '6%', 
+              transform: 'translateX(-50%)',
+              outline: 'none',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Doritos')}
+          />
+          <ImageButton 
+            src={fandangos} 
+            alt="Imagem do Fandangos"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '49%',
+              top: '6%', 
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Fandangos')} 
+          />
+          <ImageButton 
+            src={Cheetos} 
+            alt="Imagem do Cheetos"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '44%',
+              top: '6%', 
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Cheetos')} 
+          />
+        </>
+      )}
+
+      {selectedCategory === 'Bebidas' && (
+        <>
+          <ImageButton 
+            src={Monster} 
+            alt="Imagem do Monster"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '54%',
+              top: '18%',
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Monster')} 
+          />
+          <ImageButton 
+            src={Coca} 
+            alt="Imagem do Coca"
+            style={{
+              position: 'absolute',
+              width: '6%',
+              left: '49%',
+              top: '18%',
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Coca')} 
+          />
+          <ImageButton 
+            src={Pepsi} 
+            alt="Imagem do Pepsi"
+            style={{
+              position: 'absolute',
+              width: '6%',
+              left: '44%',
+              top: '20%',
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Pepsi')} 
+          />
+        </>
+      )}
+
+      {selectedCategory === 'Chocolates' && (
+        <>
+          <ImageButton 
+            src={Lacta} 
+            alt="Imagem do Lacta"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '54%',
+              top: '32%',
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Lacta')} 
+          />
+          <ImageButton 
+            src={hersheys} 
+            alt="Imagem do Hersheys"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '49%',
+              top: '32%',
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Hersheys')} 
+          />
+          <ImageButton 
+            src={garoto} 
+            alt="Imagem do Garoto"
+            style={{
+              position: 'absolute',
+              width: '5%',
+              left: '44%',
+              top: '32%',
+              transform: 'translateX(-50%)',
+            }}
+            className="image-button"
+            onClick={() => handleButtonClick('Garoto')} 
+          />
+        </>
+      )}
     </div>
-
-    
   );
 };
 
